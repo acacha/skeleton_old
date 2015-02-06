c4b758376353670bac763ffd530015e1f533f5e9
Sergi Tur <sergiturbadenas@gmail.com>
2015-02-06 09:42:33 +0100

    moved login button!

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -80,6 +80,10 @@ if (defined('ENVIRONMENT') && ENVIRONMENT=="development") {
    <script type="text/javascript" id="bootstrap_select">
         $(window).on('load', function () {
             $('.selectpicker').selectpicker();
+            $('#login_with_google').click(function() {
+                window.location.href = '<?php echo base_url('/index.php/skeleton_auth/ebre_escool_auth/login_hybrid_auth/Google');?>';
+                return false;
+            });
 		});
    </script>
 
@@ -109,9 +113,6 @@ if (defined('ENVIRONMENT') && ENVIRONMENT=="development") {
 <?php endif; ?>
      
      <center><h1><?php echo $login_appname . ". " . $login_entity;?></h1></center>
-     <form action="http://www.iesebre.com/ebre-escool/index.php/skeleton_auth/ebre_escool_auth/login_hybrid_auth/Google">
-          <center><button class="btn btn-large btn-danger" type="submit"><i class="icon-google-plus"></i> Entrar amb Google</button></center>
-      </form>       
        <?php 
        echo form_open($login_url .$redirect, array('id' => 'loginform', 'class' => 'form-signin' )); ?>
         
@@ -140,7 +141,8 @@ if (defined('ENVIRONMENT') && ENVIRONMENT=="development") {
 
         <br/>
         <button class="btn btn-large btn-primary" type="submit"><?php echo lang('Login');?></button>
-        
+        <button id="login_with_google" class="btn btn-large btn-danger" type="button"><i class="icon-google-plus"></i> Entrar amb Google</button>
+
        <?php echo form_close(); ?>
        <!--<center><p><a href="<?php echo $register_url;?>"><?php echo lang('Register');?></a></p></center>-->
        <center><p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p></center>
